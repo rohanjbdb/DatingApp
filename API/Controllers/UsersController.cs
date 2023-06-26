a38f85119c05a13f2f81ff324d8af6defb128e86
Rohan Bhingardive <rohan.bhingardive@pb.com>
2023-06-27 00:07:24 +0530

    End Of Section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,13 +1,13 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController: ControllerBase
+{   
+    [Authorize]
+    public class UsersController: BaseApiController
     {
         
         private readonly DataContext _context;
@@ -19,6 +19,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             var users = await _context.Users.ToListAsync();
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            // Add services to the container.
+            services.AddCors();
+            services.AddControllers();
+
+            services.AddDbContext<DataContext>(opt =>{
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<ITokenService,TokenService>();
+
+            return services;
+
+        }
+    }
+}
